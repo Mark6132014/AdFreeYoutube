25c5a42fce4b2646d7eebb3c5621324292ddbfc8
Mark6132014 <154909794+Mark6132014@users.noreply.github.com>
2024-08-31 09:39:34 -0400

    Delete scripts.js

--- scripts.js
+++
@@ -1,5 +0,0 @@
-let btn = document.querySelector("#submit");
-btn.addEventListener("click", () => {
-  let input = document.querySelector("#id").value;
-  location.assign(`https://www.youtube.com/embed/${input}`);
-});
