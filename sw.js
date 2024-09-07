b09699515b5fc421ecef1f02ab168301e3cc9a00
Mark6132014 <154909794+Mark6132014@users.noreply.github.com>
2024-09-07 16:12:38 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,17 +3,18 @@ self.addEventListener("install", function(event) {
   event.waitUntil(
     caches.open("sw-cache").then(function(cache) {
       // cache any static file that make up the application shell
-                                 return cache.add("index.html")
-    });
+      return cache.add("index.html");
+    })
   );
 });
+
 // On network request
 self.addEventListener("fetch", function(event) {
   event.respondWith(
     // Try the cache
     caches.match(event.request).then(function(response) {
-      // If reponse found return it, else fetch again
-                                     return response || fetch(event.request)
-    });
-);
+      // If response found return it, else fetch again
+      return response || fetch(event.request);
+    })
+  );
 });
