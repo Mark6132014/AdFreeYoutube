c95f8b2f95267a50c4265584355122ee8a087c3e
Mark6132014 <154909794+Mark6132014@users.noreply.github.com>
2024-09-07 17:50:30 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,3 +3,14 @@ let btn = document.querySelector("#submit");
         let input = document.querySelector("#id").value;
         location.assign(`video.html#${input}`);
       });
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('sw.js')
+      .then((reg) => {
+        console.log('Service worker registered:', reg);
+      })
+      .catch((error) => {
+        console.error('Service worker registration failed:', error);
+      });
+  });
+}
