c2666b51539cf9ee7ad7b8d5b8b32cc17bc408f8
Mark6132014 <154909794+Mark6132014@users.noreply.github.com>
2024-09-24 18:13:39 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,5 +2,5 @@
 let btn = document.querySelector("#submit");
       btn.addEventListener("click", () => {
         let input = document.querySelector("#id").value;
-        location.assign(`video.html#${input}`);
+        location.assign(`video.html?${input}`);
       });
